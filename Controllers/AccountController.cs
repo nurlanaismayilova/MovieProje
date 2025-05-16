e6ad85405397db2881e4c10e1de71d39e17e55f7
bmuradova17974 <Bmuradova17974@ada.edu.az>
2025-05-16 19:15:38 +0300

    adding login page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿// Controllers/AccountController.cs
+using Microsoft.AspNetCore.Mvc;
+using MovieProject.Models;
+using MovieProject.Services;
+using Microsoft.AspNetCore.Http;
+
+public class AccountController : Controller
+{
+    private readonly UserService _userService;
+
+    public AccountController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    public IActionResult SignUp() => View();
+
+    [HttpPost]
+    public IActionResult SignUp(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            _userService.Register(user);
+            return RedirectToAction("SignIn");
+        }
+        return View(user);
+    }
+
+    public IActionResult SignIn() => View();
+
+    [HttpPost]
+    public IActionResult SignIn(string username, string password)
+    {
+        var user = _userService.Authenticate(username, password);
+        if (user != null)
+        {
+            HttpContext.Session.SetString("Username", user.Username);
+            return RedirectToAction("Index", "Home");
+        }
+
+        ViewBag.Error = "Invalid credentials";
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("SignIn");
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿// Models/User.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
